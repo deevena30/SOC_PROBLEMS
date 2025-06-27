6c2d6e153ae48bf9e23e17ff9b6af2516f25ede1
Deevena Undrathi <undrathi.deevena@gmail.com>
2025-06-27 20:42:38 +0530

    Week4Soc

--- SoC_Week4Problems/w4_q2sol.cpp
+++ SoC_Week4Problems/w4_q2sol.cpp
@@ -3,18 +3,26 @@ using namespace std;
 typedef long long ll;
 const ll MOD = 1e9 + 7;
 
-ll divisorSum(int n) {
-    ll sum = 0;
-    for (ll i = 1; i <= n; ++i) {
-        ll term = ((n / i) % MOD) * (i % MOD) % MOD;
-        sum = (sum + term) % MOD;
+ll DivisorSum(int n) {
+    vector<ll> sum(n + 1, 0);
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = i; j <= n; j += i) {
+            sum[j] = (sum[j] + i) % MOD;
+        }
     }
-    return sum;
+
+    ll total = 0;
+    for (int i = 1; i <= n; ++i) {
+        total = (total + sum[i]) % MOD;
+    }
+
+    return total;
 }
 
 int main() {
     int n;
     cin >> n;
-    cout << divisorSum(n) << endl;
+    cout << DivisorSum(n) << endl;
     return 0;
 }
